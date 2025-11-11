c1afc30135f0e5dfb5a50e13f01853a1f92a7323
Nagesh Babu <nageshbabu0601@gmail.com>
2025-11-11 17:57:42 +0530

    Second Commit

--- src/com/paritech/Tuesday.java
+++ src/com/paritech/Tuesday.java
@@ -5,6 +5,7 @@ public class Tuesday {
 	public static void main(String[] args) {
 		
 		System.out.println("This is Tuesday class");
+		System.out.println("Added more lines to Tuesday class");
 
 	}
 
